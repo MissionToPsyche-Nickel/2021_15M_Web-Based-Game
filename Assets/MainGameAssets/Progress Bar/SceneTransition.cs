d10dc45e336bbfb2ac6a931a014245341472916f
ahin1 <55998952+ahin1@users.noreply.github.com>
2020-11-06 15:03:12 -0800

    Added Scene Transition Effects
    Added a transition effect between the main scene and a test scene that represents a mini-game. The effect can be tested by pressing the S key.

---
+++ Assets/MainGameAssets/Progress Bar/SceneTransition.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public int whatScene;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(LoadNewScene());
+        }
+    }
+
+    //Loads the new scene and plays the transition for it
+    private IEnumerator LoadNewScene()
+    {
+        animator.SetBool("SceneChange", true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(whatScene);
+    }
+}
