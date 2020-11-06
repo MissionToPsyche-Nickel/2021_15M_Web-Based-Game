885d10a9448673784f5704659e4d016a1db3e3f2
ahin1 <55998952+ahin1@users.noreply.github.com>
2020-11-06 12:29:58 -0800

    Added Progress Bar and animation
    Added in the progress bar along with a progress bar animation that starts when you press P and resets when you press O. This simulates focusing and losing focus with the telescope.

---
+++ Assets/MainGameAssets/Progress Bar/ProgressBarAnim.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBarAnim : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            animator.SetBool("isActive", true);
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            animator.SetBool("isActive", false);
+        }
+    }
+}
