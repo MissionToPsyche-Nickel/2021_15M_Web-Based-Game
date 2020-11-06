a66f293e1154ce91082cd4759d61e3ea0bba9cff
Jake Bement <jbement@asu.edu>
2020-11-05 16:19:46 -0800

    WEB-70 Add a Scanner object to be used for scanning from the telescope. Has a basic script that tracks the mouse movements on the screen and stops at impact with other objects. Is only a single beam.

---
+++ Assets/MainGameAssets/Scanner/ScanScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScanScript : MonoBehaviour
+{
+
+    public Camera cam;
+    public GameObject endOfTelescope;
+    public LineRenderer lineRenderer;
+    public float maximumScanningRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0, endOfTelescope.transform.position);
+        
+        RaycastHit hit;
+
+        var mousePosition = Input.mousePosition;
+        var rayMouse = cam.ScreenPointToRay(mousePosition);
+
+        if (Physics.Raycast(rayMouse.origin, rayMouse.direction, out hit, maximumScanningRange)) {
+            if (hit.collider) {
+                lineRenderer.SetPosition(1, hit.point);
+            }
+        }
+        else {
+            var position = rayMouse.GetPoint(maximumScanningRange); 
+            lineRenderer.SetPosition(1, position); }
+    }
+}
