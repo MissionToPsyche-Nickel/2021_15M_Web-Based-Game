52693ffdee46c9f3ab1c0a2f88455512eb224e4a
ahin1 <55998952+ahin1@users.noreply.github.com>
2020-11-15 15:29:37 -0800

    Added Journal Transition (In Progress)
    Adds a scene transition to the journal's default page when the journal button is clicked. Currently the transition animation is shown in the main scene, but not in the journal scene.

--- Assets/MainGameAssets/Journal/Control.cs
+++ Assets/MainGameAssets/Journal/Control.cs
@@ -6,19 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class Control : MonoBehaviour
 {
-    public int whatScene;
     public Animator animator;
+    public void Start()
+    {
 
+    }
     public void openJournalButtonClicked() {
         //Logic to find player level
         //Will load the appropriate scene
     }
     public void closeJournalButtonClicked() {
-        SceneManager.LoadScene("Main");
+        StartCoroutine(LoadNewScene(0));
     }
 
     /* This is used to go to page one if the player has more journal pages.
-    */
+     */
     public void goToPageOne() {
         SceneManager.LoadScene("JournalDefaultPage1");
     }
@@ -29,5 +31,11 @@ public class Control : MonoBehaviour
 
     public void LoadingAPageWithName(string sceneName) {
         SceneManager.LoadScene("sceneName");
+    }
+    private IEnumerator LoadNewScene(int whatScene)
+    {
+        animator.SetBool("SceneChange", true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(whatScene);
     }
 }
