9ca5850ffa2cad113f8c95874a4b554a3c5accbd
ahin1 <55998952+ahin1@users.noreply.github.com>
2020-11-25 19:35:04 -0800

    Scanner Movement (Completed)
    The scanner now moves correctly and stops when the bar is too high or too low on the screen.

--- Assets/ScanMovement.cs
+++ Assets/ScanMovement.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class ScanMovement : MonoBehaviour
 {
     public GameObject scanner;
-    public int angle = 0;
+    public int angle = 1;
     public int min = 0;
     public int max = 8;
 
     void Update()
     {
-        angle += Input.GetKey(KeyCode.DownArrow) ? 1 : 0;
-        angle += Input.GetKey(KeyCode.UpArrow) ? -1 : 0;
-        //angle = Mathf.Clamp(angle, min, max);
+        angle += Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) ? -1 : 0;
+        angle += Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) ? 1 : 0;
+        angle = Mathf.Clamp(angle, min , max);
         scanner.transform.eulerAngles = new Vector3(0, 0, angle);
     }
 }
