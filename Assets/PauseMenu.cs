964d5589388cc70ea19720171de7605c09a8e0d7
Vignesh <vigneshs96@yahoo.com>
2020-12-04 01:50:43 -0800

    add resume,menu,quit button with script

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+        
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKeyDown(KeyCode.Escape))
+    {
+        if (GameIsPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+    }
+void Pause()
+{
+    pauseMenuUI.setActive(true);
+    Time.timeScale = 1f;
+    GameIsPaused = false;
+}
+void Resume()
+{
+    pauseMenuUI.setActive(false);
+    Time.timeScale = 0f;
+    GameIsPaused = true;
+}
+
+}
