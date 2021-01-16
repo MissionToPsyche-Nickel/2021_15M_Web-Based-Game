7437fbbfb9d08ff07fc51b032434fbfda9a0a87a
Jake Bement <jbement@asu.edu>
2021-01-16 08:09:19 -0800

    WEB-114 Basic logic for word match mini game.

--- Assets/MainGameAssets/WordMatchGame/WordMatchControl.cs
+++ Assets/MainGameAssets/WordMatchGame/WordMatchControl.cs
@@ -1,71 +1,121 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WordMatchControl : MonoBehaviour
 {
     // Simple lock to prevent multiple word presses. When a word is clicked this is set to true. On clicking a definition will check if this is true before painting line.
-    public bool wordWasClicked;
+    private static bool wordWasClicked;
     private Transform[] points;
 
     public GameObject word1;
     public GameObject definition1;
 
     // The word and definition objects
-    private int[] selectedPair;
-    private int[] playerChoices;
-    private int[] answerKey;
+    private static int selection;
+    private static int[] playerChoices;
+    private int[] answerKey = new int[] {1,1,2,2,3,3};
     
     // To create a line from current word to definition
-    private LineRenderer lineRenderer;
-    private Vector3 startPosition;
+    //private LineRenderer lineRenderer;
+    private Transform startPosition;
     private Vector3 mousePosition;
-    private Vector3 endPosition;
+    private Transform endPosition;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         wordWasClicked = false;
-        lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.enabled = false;
-
-        // Simple arrays to hold player choices and answer key
-        selectedPair = new int[2] {0,0};
-        playerChoices = new int[6] {0,0,0,0,0,0};
-        answerKey = new int[] {1,1,0,0,0,0};
+        selection = 0;
+        playerChoices = new int[6] {1,0,2,0,3,0};
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    
+        // Track mouse position after player click
+        //mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);  
     }
 
-    public void wordClicked(int wordNum) {
-        Debug.Log("Word" + wordNum + " clicked.");
-
-        // Set this value as first pair value and clear second pair value
-        selectedPair[0] = wordNum;
-        selectedPair[1] = 0;
-
+    public void word1Clicked() {
+        Debug.Log("Word 1 clicked.");
         
+        //this acts as a lock to prevent multiple definitions from being selected.
         wordWasClicked = true;
-        startPosition = GameObject.Find("word" + wordNum).transform.position;
-        lineRenderer.SetPosition(0, startPosition);
-        lineRenderer.SetPosition(1, mousePosition);   
+        selection = 1;
+
+        /**
+        startPosition = word1.transform;
+        
+        if (lineRenderer == null) {
+            lineRenderer = gameObject.AddComponent<LineRenderer>();
+        }
+        lineRenderer.SetPosition(0, startPosition.position);
+        lineRenderer.SetPosition(1, mousePosition);  
+        lineRenderer.useWorldSpace = true; 
 
         if (!lineRenderer.enabled) {
             lineRenderer.enabled = true;
         }
-     
-        
-        // Set start position as edge of this object and then draw line
-        //makeLine();
+        */  
+    }
+
+    public void word2Clicked() {
+        Debug.Log("Word 2 clicked.");
+        wordWasClicked = true;
+        selection = 2;
+    }
+
+    public void word3Clicked() {
+        Debug.Log("Word 3 clicked.");
+        wordWasClicked = true;
+        selection = 3;
     }
 
     public void definitionClicked(int defNum) {
-        Debug.Log("Definition" + defNum + " clicked");
+        if (wordWasClicked) {
+            Debug.Log("Definition " + defNum + " clicked while word " + selection + " selected.");
+
+            switch (selection) {
+
+                case 1:
+                playerChoices[1] = defNum;
+                break;
+
+                case 2:
+                playerChoices[3] = defNum;
+                break;
+
+                case 3:
+                playerChoices[5] = defNum;
+                break;
+            }
+        }
+        else {
+            Debug.Log("Definition clicked when no word was selected.");
+        } 
+
+        wordWasClicked = false;
+
+        //lineRenderer.enabled = false;
+    }
+
+    public void checkAnswers() {
+        if (!wordWasClicked) {
+            bool isCorrect = true;
+            for (int i = 0; i < answerKey.Length; i++) {
+                if (playerChoices[i] != answerKey[i]) {
+                    isCorrect = false;
+                }
+            }
+            if (isCorrect) {
+                Debug.Log("All answers are correct.");
+            } else {
+                Debug.Log("Wrong.");
+            }
+        }
+        else {
+            Debug.Log("Tried to check answers when word was selected.");
+            //Give reminder to player to make a selection
+        }
     }
 }
