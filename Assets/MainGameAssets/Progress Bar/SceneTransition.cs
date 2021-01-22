04d9e98065d27374cbe8c3f05ec510694286b9f6
ahin1 <55998952+ahin1@users.noreply.github.com>
2021-01-22 13:25:06 -0800

    Updated Scene Transition Code
    Updated the Scene Transition Code so that the name of a scene can be input instead of an int. The correct int index for each scene will still need to be placed into the code, since Unity uses ints to refer to scenes.

--- Assets/MainGameAssets/Progress Bar/SceneTransition.cs
+++ Assets/MainGameAssets/Progress Bar/SceneTransition.cs
@@ -19,20 +19,46 @@ public class SceneTransition : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.T))
         {
-            StartCoroutine(LoadNewScene());
+            StartCoroutine(LoadNewScene("Mini-game Test"));
         }
     }
 
     public void SceneTransitionOnClick()
     {
-        StartCoroutine(LoadNewScene());
+        StartCoroutine(LoadNewScene("Mini-game Test"));
     }
 
     //Loads the new scene and plays the transition for it
-    private IEnumerator LoadNewScene()
+    private IEnumerator LoadNewScene(string sceneName)
     {
+        string theSceneName = sceneName;
+        whatScene = setScene(theSceneName);
         animator.SetBool("SceneChange", true);
         yield return new WaitForSeconds(1.5f);
         SceneManager.LoadScene(whatScene);
+    }
+
+    private int setScene(string sceneName)
+    {
+        if (sceneName.Equals("Main Menu"))
+        {
+            return 1;
+        }
+        else if (sceneName.Equals("Main Gameplay Screen"))
+        {
+            return 0;
+        }
+        else if (sceneName.Equals("Sliding Puzzle Game"))
+        {
+            return 2;
+        }
+        else if (sceneName.Equals("Memory Match Game"))
+        {
+            return 3;
+        }
+        else
+        {
+            return 0;
+        }
     }
 }
