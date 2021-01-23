f5c857d6a025cba514c30c5b5c7382138cdb4e14
Vignesh <vigneshs96@yahoo.com>
2021-01-22 20:56:19 -0800

    Add test for sliding puzzle for Loading Texture

---
+++ Assets/MainGameAssets/Tests/TestSuite.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.Assertions;
+
+namespace Tests
+{   private 
+    public class TestSuite
+    {
+        
+        [UnityTest]
+        public IEnumerator TestLoadTexture()
+        {// expected result to compare in test
+            LoadTexture expected = new LoadTexture(AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/MainGameAssets/SpaceFront.png"));
+            // Create a load texture function and load the same picture
+            LoadTexture test= new  LoadTexture(AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/MainGameAssets/SpaceFront.png"));
+            // now use an assert testing function to see if two objects are equal
+            Assert.AreEqual(test, expected);
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+        }
+    }
+}
