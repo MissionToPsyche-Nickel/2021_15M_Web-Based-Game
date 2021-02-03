b799e038c62ce617b8cacabd201b3d2afd561636
Mark Sauve <msauve2@asu.edu>
2021-02-02 22:05:37 -0600

    Make the twinkle appear at a random spot

--- Assets/MainGameAssets/Twinkle.cs
+++ Assets/MainGameAssets/Twinkle.cs
@@ -7,7 +7,13 @@ public class Twinkle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(
+            Random.Range(0.0f, 8.0f),
+            Random.Range(-4.0f, 4.0f),
+            -1
+        );
+
+        // visibility modifier based on difficulty probably goes here
     }
 
     // Update is called once per frame
