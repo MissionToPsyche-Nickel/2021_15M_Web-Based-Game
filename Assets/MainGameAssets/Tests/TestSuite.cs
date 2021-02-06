ac34ba96d71e61602b55fc6e901b7bb94402f204
Vignesh <vigneshs96@yahoo.com>
2021-02-05 21:03:25 -0800

    delete all tests, add correct imports and Nunit framework, and create dummy test to test if it passes

--- Assets/MainGameAssets/Tests/TestSuite.cs
+++ Assets/MainGameAssets/Tests/TestSuite.cs
@@ -1,31 +1,32 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-using UnityEngine.Assertions;
+using UnityEngine.Assertions;*/
+
+using NUnit.Framework;
+using Is = NUnit.Framework.Is;
 
 namespace Tests
 {   
     public class TestSuite
     {
-        //create a unit testing case for loading texture
-        [UnityTest]
-        public IEnumerator TestLoadTexture()
-        {// expected result to compare in test
-            LoadTexture expected = new LoadTexture(AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/MainGameAssets/SpaceFront.png"));
-            // Create a load texture function and load the same picture
-            LoadTexture test= new  LoadTexture(AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/MainGameAssets/SpaceFront.png"));
-            // now use an assert testing function to see if two objects are equal
-            Assert.AreEqual(test, expected);
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-        }
-        [UnityTest]
-        public IEnumerator TestLoadTexture()
-        {// Second test case
-            yield return null;
+        /*        [SetUp]
+                public void ResetScene()
+                {
+
+                    EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+
+                }*/
+        [Test]
+        public void BasicTest()
+        {
+
+            bool isActive = false;
+
+            Assert.AreEqual(false, isActive);
+
         }
     }
 }
