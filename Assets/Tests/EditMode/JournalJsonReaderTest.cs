fa5a54297051cea1a2aec573bceeda4f15dd6614
Jake Bement <jbement@asu.edu>
2021-02-12 13:23:53 -0800

    WEB-174 add deserialization script for journal entry JSON file.

---
+++ Assets/Tests/EditMode/JournalJsonReaderTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class JournalJsonReaderTest
+    {
+        JournalJsonReader jsonReader;
+        
+        [SetUp]
+        void SetUp()
+        {
+            jsonReader = new GameObject().AddComponent<JournalJsonReader>();
+            jsonReader.jsonFile = Resources.Load("journal") as TextAsset;
+        }
+
+        [Test]
+        public void JournalJsonReaderClassExists()
+        {
+            //Object exists
+            Assert.IsNotNull(jsonReader);
+            //Object has the correct json file attached
+            Assert.IsNotNull(jsonReader.jsonFile);
+        }
+
+        [UnityTest]
+        public IEnumerator TestStart()
+        {
+            Assert.Pass("Ignore stack trace");
+            yield return null;
+        }
+
+        [Test]
+        public void JournalFileParsesTest()
+        {
+            Pages pageList = jsonReader.LoadPagesFromFile();
+            
+            Assert.IsNotNull(pageList);
+        }
+        
+    }
+}
