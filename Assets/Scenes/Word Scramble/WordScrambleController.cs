904263c510da38075bed8330938bbcc619896e1a
ahin1 <55998952+ahin1@users.noreply.github.com>
2021-03-03 12:54:58 -0800

    Added text input to Word Scramble Game
    Added text input for each scrambled word. Text is auto-capitalized, and you can only type in numbers and letters up to the number of letters in each word.

---
+++ Assets/Scenes/Word Scramble/WordScrambleController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordScrambleController : MonoBehaviour
+{
+    public InputField InputField1;
+    public InputField InputField2;
+    public InputField InputField3;
+    public InputField InputField4;
+    public InputField InputField5;
+
+    public void Start()
+    {
+        InputField1.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        InputField2.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        InputField3.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        InputField4.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        InputField5.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+    public void ValueChangeCheck()
+    {
+        InputField1.GetComponent<InputField>().text = InputField1.GetComponent<InputField>().text.ToUpper();
+        InputField2.GetComponent<InputField>().text = InputField2.GetComponent<InputField>().text.ToUpper();
+        InputField3.GetComponent<InputField>().text = InputField3.GetComponent<InputField>().text.ToUpper();
+        InputField4.GetComponent<InputField>().text = InputField4.GetComponent<InputField>().text.ToUpper();
+        InputField5.GetComponent<InputField>().text = InputField5.GetComponent<InputField>().text.ToUpper();
+    }
+}
