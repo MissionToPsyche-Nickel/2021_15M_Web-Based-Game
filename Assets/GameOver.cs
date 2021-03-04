58eccbb9cd29c57755f7f7bbbc4bc227ae0c17b5
Vignesh <vigneshs96@yahoo.com>
2021-03-03 22:47:20 -0800

    add script for game over

---
+++ Assets/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{       public void setup(int state)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
