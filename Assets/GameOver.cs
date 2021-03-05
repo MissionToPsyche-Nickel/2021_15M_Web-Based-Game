a6db1d17de3bcb2f2800a2eeb7406113ca0e578b
Vignesh <vigneshs96@yahoo.com>
2021-03-04 23:00:21 -0800

    game over script completed

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
-{       public void setup(int state)
+{    /*   public void setup(int state)
     {
 
-    }
+    }*/
     // Start is called before the first frame update
-    void Start()
+  /*  void Start()
     {
         
     }
@@ -17,5 +20,20 @@ public class GameOver : MonoBehaviour
     void Update()
     {
         
+    }*/
+
+    public void RestartClick()
+    {
+        SceneManager.LoadScene("Main");
+        Debug.Log("Main Menu Clicked...");
+
     }
+
+    public void ExitClick()
+    {
+        SceneManager.LoadScene("Title");
+        Debug.Log("Exit Clicked...");
+
+    }
+
 }
