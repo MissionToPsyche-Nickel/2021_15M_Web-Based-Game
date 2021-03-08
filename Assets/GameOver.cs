3059d1218bf0ef10282cdd3faaeac09939b32ca1
vsubra35 <62535724+vsubra35@users.noreply.github.com>
2021-03-08 05:12:53 -0800

    Vsubra35 sprint9 (#6)
    * Add canvas with image for end scene
    
    * Add two buttons to Game Over Scene
    
    * add script for game over
    
    * game over script completed
    
    * fixed start menu
    
    Co-authored-by: Vignesh <vigneshs96@yahoo.com>

---
+++ Assets/GameOver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{    /*   public void setup(int state)
+    {
+
+    }*/
+    // Start is called before the first frame update
+  /*  void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
+    public void RestartClick()
+    {
+        SceneManager.LoadScene("Main");
+        Debug.Log("Main Menu Clicked...");
+
+    }
+
+    public void ExitClick()
+    {
+        SceneManager.LoadScene("Title");
+        Debug.Log("Exit Clicked...");
+
+    }
+
+}
